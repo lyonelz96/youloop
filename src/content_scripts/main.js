a8fa84c67b7791702614ea11bd6a3e3c5eced7de
Lyonel Zamora <lyonelz516@gmail.com>
2022-10-14 15:26:30 -0400

    fix toggle not working when first visiting youtube

--- src/content_scripts/main.js
+++ src/content_scripts/main.js
@@ -2,12 +2,16 @@ const { GlobalUtils } = require('../../src/utils');
 const { Components } = require('./components/main');
 
 async function init() {
-    const enabled = await GlobalUtils.getLoopDefaultEnable().catch(
-        GlobalUtils.errorLogger
-    );
+    const { pathname, searchParams } = new URL(document.URL);
 
-    if (enabled && !Components.Container.Utils.get()) {
-        Components.Container.Utils.insert();
+    if (pathname === '/watch' && searchParams.has('v')) {
+        const enabled = await GlobalUtils.getLoopDefaultEnable().catch(
+            GlobalUtils.errorLogger
+        );
+
+        if (enabled && !Components.Container.Utils.get()) {
+            Components.Container.Utils.insert();
+        }
     }
 
     Components.Container.Listeners.OnMessage.Toggle();
