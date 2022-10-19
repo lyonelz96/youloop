481d733616419861f597ace79918e974eaa3e7db
Lyonel Zamora <lyonelz516@gmail.com>
2022-10-19 12:08:19 -0400

    add esmify

--- scripts/browserify.sh
+++ scripts/browserify.sh
@@ -1,2 +1,2 @@
-npx browserify './src/content_scripts/main.js' -o './src/content_scripts/bundle.js'
-npx browserify './src/page_action/popup/main.js' -o './src/page_action/popup/bundle.js'
+npx browserify './src/content_scripts/main.js' -p esmify -o './src/content_scripts/bundle.js'
+npx browserify './src/page_action/popup/main.js' -p esmify -o './src/page_action/popup/bundle.js'
